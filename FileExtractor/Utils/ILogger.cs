fa510d820ed60c5ba6e5789db4301810c1a42dbf
Volodymyr Handziuk <gandziuk2009@gmail.com>
2022-02-03 22:45:25 +0100

    minor refactoring

--- FileExtractor/Utils/ILogger.cs
+++ FileExtractor/Utils/ILogger.cs
@@ -1,13 +1,12 @@
-namespace FileExtractor.Utils
+namespace FileExtractor.Utils;
+
+internal interface ILogger<T>
 {
-    internal interface ILogger<T>
-    {
-        void Information(string messageTemplate);
-        void Information(string messageTemplate, params object[] propertyValues);
+    void Information(string messageTemplate);
+    void Information(string messageTemplate, params object[] propertyValues);
 
-        void Warning(string messageTemplate);
-        void Warning(string messageTemplate, params object[] propertyValues);
+    void Warning(string messageTemplate);
+    void Warning(string messageTemplate, params object[] propertyValues);
 
-        void Error(Exception exception, string messageTemplate, params object[] propertyValues);
-    }
+    void Error(Exception exception, string messageTemplate, params object[] propertyValues);
 }
