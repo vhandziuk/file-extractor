f4b967a6485a54f4a1e9e602d26d57bd1412c8e7
Volodymyr Handziuk <gandziuk2009@gmail.com>
2022-02-06 00:36:02 +0100

    added support for file-based log output; minor refactoring

--- FileExtractor/Utils/SerilogLoggerFactory.cs
+++ FileExtractor/Utils/SerilogLoggerFactory.cs
@@ -1,19 +1,37 @@
+using System.Text;
 using Serilog;
 using Serilog.Core;
+
+using SpecialFolder = System.Environment.SpecialFolder;
 
 namespace FileExtractor.Utils;
 
 internal static class SerilogLoggerFactory
 {
-    private const string OutputTemplate =
-        "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [{ProcessId}] [{ThreadId}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+    private const string EventTimingAndLevel = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}]";
+    private const string EventDetails = "{Message:lj}{NewLine}{Exception}";
+
+    private const string ConsoleOutputTemplate = $"{EventTimingAndLevel} {EventDetails}";
+    private const string FileOutputTemplate = $"{EventTimingAndLevel} [{{ProcessId}}] [{{ThreadId}}] [{{SourceContext}}] {EventDetails}";
+
+    private static readonly string _logFilePath = Path.Combine(
+        Environment.GetFolderPath(SpecialFolder.CommonApplicationData), "FileExtractor", "Application.log");
 
     private static readonly Lazy<ILogger> _logger =
         new Lazy<ILogger>(() =>
             new LoggerConfiguration()
                 .Enrich.WithProcessId()
                 .Enrich.WithThreadId()
-                .WriteTo.ColoredConsole(outputTemplate: OutputTemplate)
+                .WriteTo.ColoredConsole(
+                    outputTemplate: ConsoleOutputTemplate)
+                .WriteTo.File(
+                    path: _logFilePath,
+                    outputTemplate: FileOutputTemplate,
+                    fileSizeLimitBytes: 100_000L,
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: 5,
+                    encoding: Encoding.Unicode
+                )
                 .CreateLogger());
 
     public static ILogger Create<T>() =>
