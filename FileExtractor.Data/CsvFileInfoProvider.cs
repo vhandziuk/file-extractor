c064becf0aa9ae57f82df80cac5fdb694796798e
Volodymyr Handziuk <gandziuk2009@gmail.com>
2023-08-05 22:52:02 +0200

    adjusted CSV configuration file structure

--- FileExtractor.Data/CsvFileInfoProvider.cs
+++ FileExtractor.Data/CsvFileInfoProvider.cs
@@ -2,33 +2,34 @@ namespace FileExtractor.Data;
 
 public sealed class CsvFileInfoProvider : ICsvFileInfoProvider
 {
+    private static readonly HashSet<int> AllowedColumnCounts = new() { 1, 2, 3 };
+
     public IEnumerable<FileInfoData> EnumerateEntries(string filePath)
     {
-        using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-        using (var reader = new StreamReader(stream))
+        using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        using var reader = new StreamReader(stream);
+
+        while (reader.ReadLine() is string line)
         {
-            while (reader.ReadLine() is string line)
+            var data = line.Split(',');
+            if (!AllowedColumnCounts.Contains(data.Length))
             {
-                var data = line.Split(',');
-                if (data.Length != 2)
-                {
-                    throw new Exception(
-                        "Malformed configuration file. " +
-                        "Data must contain 2 columns containing file name " +
-                        "and [optionally] subfolder path or an empty space separated by a comma");
-                }
-
-                var directory = Path.GetDirectoryName(data[0]) ?? string.Empty;
-                var name = Path.GetFileName(data[0]);
-                var location = data[1];
-
-                if (string.IsNullOrWhiteSpace(name))
-                {
-                    throw new Exception("File name cannot be empty or whitespace");
-                }
-
-                yield return new FileInfoData(directory, name, location);
+                throw new Exception(
+                    "Malformed configuration file. " +
+                    "Data must contain 1, 2, or 3 columns containing [required] file name, [optional] extraction" +
+                    "subfolder, and [optional] archive path (subfolder in which the file is located)");
             }
+
+            var name = data[0];
+            var directory = data.ElementAtOrDefault(1) ?? string.Empty;
+            var location = data.ElementAtOrDefault(2) ?? string.Empty;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("File name cannot be empty or whitespace");
+            }
+
+            yield return new FileInfoData(directory, name, location);
         }
     }
 }
